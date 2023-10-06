63aafc04ff81e8b2a53f5f739cb66fc632199c83
JulV13 <julv1301@gmail.com>
2023-10-07 00:19:38 +0200

    one more solution

---
+++ making_a_box/making_a_box/making_a_box.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void makeBox(int n) {
+   
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 || i == n - 1) {
+                cout << "#";
+            }
+            else if (j == 0 || j == n-1){
+                cout << "#";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int n = 0;
+    int m = 0;
+    cout << "Insert 'n' for a box: ";
+    cin >> n;
+    m = n;
+    makeBox(n);
+}
+
