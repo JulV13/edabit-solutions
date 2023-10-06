6321c0a2075ee65b81a30f7733a78926ac462639
JulV13 <julv1301@gmail.com>
2023-10-07 00:35:35 +0200

    another one

---
+++ multiplication_table/multiplication_table/multiplication_table.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void multiplicationTable(int n) {
+    for (int i = 1; i < n+1; i++) {
+        for (int j = 1; j < n+1; j++) {
+            if (j == n) {
+                cout << i * j;
+            }
+            else {
+                cout << i * j << ", ";
+            }
+        }
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int n = 0;
+    cout << "Insert a N parameter: ";
+    cin >> n;
+    cout << "\n";
+    multiplicationTable(n);
+}
+
