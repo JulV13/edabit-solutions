38f552793ba70e21840676233590881561dcf901
JulV13 <julv1301@gmail.com>
2023-10-09 22:08:12 +0200

    reverse coding challenge

---
+++ reverse_coding_challenge/reverse_coding_challenge/reverse_coding_challenge.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void reverseCoding(string text) {
+    for (int i = 0; i < text.size(); i+=2) {
+        char letter = text[i];
+        string helper(1, text[i + 1]);
+        int test = stoi(helper);
+        for (int j = 0; j < test; j++) {
+            cout << letter;
+        }
+    }
+}
+
+int main()
+{
+    string text = "";
+    cout << "Input text: ";
+    cin >> text;
+    cout << "Output: ";
+    reverseCoding(text);
+}
