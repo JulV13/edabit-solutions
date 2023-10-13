1b93f32977ea02c99783e0a18253af18929cc7b0
JulV13 <julv1301@gmail.com>
2023-10-13 17:30:23 +0200

    christmas tree

---
+++ christmas_tree/christmas_tree/christmas_tree.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void DrawChristmasTree(int h) {
+    int blank = 3 * (h / 2) - 1;
+    int stars = 1;
+    for (int i = 0; i < h; i++) {
+        for (int s = 0; s < blank; s++) {
+            cout << " ";
+        }
+        for (int g = 0; g < stars; g++) {
+            cout << "*";
+        }
+        for (int p = 0; p < blank; p++) {
+            cout << " ";
+        }
+        blank -= 1;
+        stars += 2;
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    int height = 0;
+    cout << "Insert height of your christmas tree: ";
+    cin >> height;
+    DrawChristmasTree(height);
+}
+
